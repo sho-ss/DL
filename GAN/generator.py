1426d68ff4b8f234a114d8cde0a8b917e5ea3ae8
syo-ss <gasta.win@gmail.com>
2018-07-26 11:52:02 +0900

    add generator add ReluNet

--- GAN/generator.py
+++ GAN/generator.py
@@ -11,14 +11,11 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 
-class Generator():
-	def __init__(self, n_in, n_out, n_hidden, batch_size=100, every_eval=10, logger=None):
+class ReluNet():
+	def __init__(self, n_in, n_out, n_hiddens):
 		self.n_in = n_in
 		self.n_out = n_out
-		self.n_hidden = n_hidden
-		self.batch_size = batch_size
-		self.every_eval = every_eval
-		self.logger = logger
+		self.n_hiddens = n_hiddens
 
 	def _weight_variable(self, shape):
 		initializer = tf.contrib.layers.xavier_initializer_conv2d()
@@ -28,7 +25,7 @@ class Generator():
 		initializer = tf.contrib.layers.xavier_initializer_conv2d()
 		return tf.Variable(initializer(shape=shape))
 
-	def _generator_layer(self, input_tensor, layer_name, n_in, n_out, act=tf.nn.relu):
+	def _generator_layer(self, input_tensor, layer_name, n_in, n_out, act=tf.nn.relu, apply_bn=True):
 		with tf.name_scope(layer_name):
 			with tf.name_scope("W"):
 				W = self._weight_variable(shape=[n_in, n_out])
@@ -36,28 +33,40 @@ class Generator():
 				b = self._bias_variable(shape=[n_out])
 			with tf.name_scope("Wx_plus_b"):
 				z = tf.matmul(input_tensor, W) + b
+			if apply_bn:
+				with tf.name_scope("batch_norm"):
+					z_bn = tf.layers.batch_normalization(z, training=self.is_training)
 			with tf.name_scope("activation"):
-				a = act(z)
+				if apply_bn:
+					a = act(z_bn)
+				else:
+					a = act(z)
 			return a
 
 	def inference(self):
 		# create 3layer NN
-		n_layer = 3
-		units = [self.n_in, self.n_hidden, self.n_out]
+		units = [self.n_in]
+		units.extend(self.n_hiddens)
+		units.append(self.n_out)
+		n_layer = len(units) - 1
 		with tf.name_scope("Generator"):
 			# define placeholder
-			self.x = tf.placeholder(tf.float32, shape=[None, self.n_in])
+			self.x = tf.placeholder(tf.float32, shape=[None, self.n_in], name="input")
+			# used for batch normalization
+			self.is_training = tf.placeholder(tf.bool, name="bn_fool")
 			input_tensor = self.x
 			activation = None
-			for i in range(n_layer - 1):
+			for i in range(n_layer):
 				# only last layer's act func is sigmoid
 				# other relu
-				if i == (n_layer - 2):
+				if i == (n_layer - 1):
 					act = tf.nn.sigmoid
+					apply_bn = False
 				else:
-					act = tf.nn.relu
+					act = tf.nn.leaky_relu
+					apply_bn = True
 				layer_name = "layer" + str(i)
 				activation = self._generator_layer(input_tensor, layer_name=layer_name,
-					n_in=units[i], n_out=units[i + 1], act=act)
+					n_in=units[i], n_out=units[i + 1], act=act, apply_bn=apply_bn)
 				input_tensor = activation
 		return activation

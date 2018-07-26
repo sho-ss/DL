9336609cc2c000fc552b2541207f38a0fe5bd601
syo-ss <gasta.win@gmail.com>
2018-07-26 14:14:31 +0900

    add smothing to check train process output gradient output loss Discriminator & Generator add loss of D & G to summary

--- GAN/GAN.py
+++ GAN/GAN.py
@@ -17,7 +17,7 @@ tf.logging.set_verbosity(old_v)
 
 
 class GAN():
-	def __init__(self, dropout, n_hiddens_generator, discriminator_iter=1, every_eval=10):
+	def __init__(self, dropout, n_hiddens_generator, discriminator_iter=1, lr=.0002, every_eval=10):
 		"""
 		@param n_hiddens_generator list
 			num of generator's hidden unit per layer
@@ -35,6 +35,9 @@ class GAN():
 		self.n_hiddens_generator = n_hiddens_generator
 
 		self.discriminator_iter = discriminator_iter
+
+		# learning rate
+		self.lr = lr
 
 		# evaluation each every_eval step
 		self.every_eval = every_eval
@@ -64,16 +67,10 @@ class GAN():
 		xs, _ = mnist.train.next_batch(sample_size)
 		return xs
 
-	def __likelihood(self, logits_d_data, logits_d_gen, sample_size):
-		with tf.name_scope("likelihood"):
-			likelihood = tf.reduce_mean(tf.reduce_sum(tf.log(logits_d_data) +
-				tf.log(tf.ones([sample_size, 1], tf.float32) - logits_d_gen), axis=1))
-		return likelihood
-
-	def __loss_discriminator(self, logits_d_data, logits_d_gen, sample_size):
+	def __loss_discriminator(self, logits_d_data, logits_d_gen, size):
 		with tf.name_scope("loss_discriminator"):
 			loss = tf.reduce_mean(-tf.reduce_sum(tf.log(logits_d_data) +
-				tf.log(tf.ones([sample_size, 1], tf.float32) - logits_d_gen), axis=1))
+				tf.log(tf.ones([size, 1], tf.float32) - logits_d_gen), axis=1))
 		return loss
 
 	def __loss_generator(self, logits_d_gen):
@@ -83,13 +80,23 @@ class GAN():
 
 	def __train_step(self, loss, var_list, name):
 		with tf.name_scope(name):
-			train_step = tf.train.AdamOptimizer(0.0002).minimize(loss, var_list=var_list)
+			train_step = tf.train.GradientDescentOptimizer(self.lr).minimize(loss, var_list=var_list)
 		return train_step
 
 	def __mean_p_data(self, logits_d_data):
 		with tf.name_scope("mean_p_data"):
 			p_data = tf.reduce_mean(logits_d_data)
 		return p_data
+
+	def __print_grads(self, grads_d_val, grads_g_val, d_vars, g_vars):
+		print("#gradient_of_D")
+		for v, val in zip(d_vars, grads_d_val):
+			norm = np.linalg.norm(val)
+			print(v.name + ": ", norm)
+		print("#gradient_of_G")
+		for v, val in zip(g_vars, grads_g_val):
+			norm = np.linalg.norm(val)
+			print(v.name + ": ", norm)
 
 	def train(self, sample_size, max_iter, every_samples):
 		# used for ploting generative samples
@@ -110,6 +117,8 @@ class GAN():
 
 			# input placeholder
 			x_data = tf.placeholder(tf.float32, shape=(None, generator_dim))
+			# used for cal loss_D
+			size = tf.placeholder(tf.int32)
 
 			# inference discriminator and generator
 			logits_g = generator.inference()
@@ -121,8 +130,10 @@ class GAN():
 			tf.summary.scalar("prob_data", p_data)
 
 			# loss discriminator and generator
-			loss_d = self.__loss_discriminator(logits_d_data, logits_d_gen, sample_size)
+			loss_d = self.__loss_discriminator(logits_d_data, logits_d_gen, size)
 			loss_g = self.__loss_generator(logits_d_gen)
+			tf.summary.scalar("loss_D", loss_d)
+			tf.summary.scalar("loss_G", loss_g)
 
 			# get discriminator vars
 			discriminator_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="Discriminator")
@@ -135,13 +146,17 @@ class GAN():
 			# get batch norm ops
 			extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
+			# used for checking training failure or not
+			grads_d = tf.gradients(loss_d, discriminator_vars)
+			grads_g = tf.gradients(loss_g, generator_vars)
+			grad_check_size = 100
+			grad_check_data = Xs[0:grad_check_size]
+			grad_check_noise = self.__sample_noize(grad_check_size)
+
 			# create test data
 			test_size = 10000
 			test_noise = self.__sample_noize(test_size)
 			test_data = mnist.test.images[0: test_size]
-			# likelihood of GAN
-			likelihood = self.__likelihood(logits_d_data, logits_d_gen, test_size)
-			tf.summary.scalar("likelihood", likelihood)
 
 			# create data for plotting
 			num_plot = self.plot_cols * self.plot_rows
@@ -171,6 +186,7 @@ class GAN():
 							feed_dict={x_data: sample_data,
 										generator.x: sample_noise,
 										generator.is_training: False,
+										size: sample_size,
 										discriminator.keep_prob: 1.0 - discriminator.dropout})
 
 					# update step of generator
@@ -182,13 +198,25 @@ class GAN():
 
 					# eval likelihood
 					if i % self.every_eval == 0:
-						summary, like_val, p_val = sess.run([merged, likelihood, p_data],
+						# eval norm of gradients
+						grads_d_val, grads_g_val = sess.run([grads_d, grads_g],
+							feed_dict={x_data: grad_check_data,
+										generator.x: grad_check_noise,
+										generator.is_training: False,
+										size: grad_check_size,
+										discriminator.keep_prob: 1.0})
+						print("##################################################")
+						self.__print_grads(grads_d_val, grads_g_val, discriminator_vars, generator_vars)
+						# eval test stats
+						summary, loss_d_tmp, loss_g_tmp = sess.run([merged, loss_d, loss_g],
 							feed_dict={x_data: test_data,
 										generator.x: test_noise,
 										generator.is_training: False,
+										size: test_size,
 										discriminator.keep_prob: 1.0})
 						writer.add_summary(summary, i)
-						print("epoch: {}, likelihood: {:.3f}, Pdata: {:.3f}".format(i, like_val, p_val))
+						print("epoch: {}, loss D: {:.3f}, loss G: {:.3f}".format(i, loss_d_tmp, loss_g_tmp))
+						print("##################################################")
 
 					if i % self.every_samples == 0:
 						epochs.append(i)
@@ -232,8 +260,8 @@ def main():
 	logger.setLevel(logging.DEBUG)
 
 	tf.reset_default_graph()
-	gan = GAN(dropout=0.5, n_hiddens_generator=[256], every_eval=1000)
-	gan.train(sample_size=50, max_iter=20000, every_samples=1000)
+	gan = GAN(dropout=0.5, n_hiddens_generator=[256], lr=0.001, every_eval=5000)
+	gan.train(sample_size=50, max_iter=100000, every_samples=5000)
 
 
 def test():

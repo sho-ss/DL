520f737c255d115d25e18693c6a6b15007930d01
syo-ss <gasta.win@gmail.com>
2018-07-30 07:36:22 +0900

    modify Discrim cnn change way of def variable batch norm layers -> nn don't use moving ave and variance

--- GAN/models/Discriminators/cnn_only_conv.py
+++ GAN/models/Discriminators/cnn_only_conv.py
@@ -5,18 +5,23 @@ import shutil
 
 
 class CNNonlyConv():
-	def __init__(self, h_in, w_in, dim_in):
-		# inputs dim
-		self.h_in = h_in
-		self.w_in = w_in
-		self.dim_in = dim_in
-		self.dim_layer1 = 16
-		self.dim_layer2 = 32
-		self.dim_layer3 = 64
-		self.filter_size = 3
+	def __init__(self, out_dim=1, channels=[1, 64, 128, 256], widths=[28, 10, 5, 3], kernels=[3, 2, 2]):
+		# convolution's channels
+		self.in_channels = channels[:-1]
+		self.out_channels = channels[1:]
+		self.n_layer = len(channels) - 1
+
+		# image widths each layer
+		self.widths = widths
+		# conv's kernel size also stride size too
+		self.kernels = kernels
+
+		# output dim
+		self.out_dim = out_dim
 
 		# define param initializer
-		self.initializer = tf.random_normal_initializer(mean=0.0, stddev=0.02)
+		self.w_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.02)
+		self.b_initializer = tf.zeros_initializer()
 
 		# define keep prob
 		self.keep_prob = tf.placeholder(tf.float32, name="keep_prob")
@@ -24,55 +29,50 @@ class CNNonlyConv():
 		# define batch norm trainable var
 		self.is_training = tf.placeholder(tf.bool, name="bn_bool")
 
-		# initialize filter
-		with tf.name_scope("filter_of_conv0"):
-			self.W1 = self._weight_variable(shape=[self.filter_size, self.filter_size,
-				self.dim_in, self.dim_layer1])
-		with tf.name_scope("filter_of_conv1"):
-			self.W2 = self._weight_variable(shape=[self.filter_size, self.filter_size,
-				self.dim_layer1, self.dim_layer2])
-		with tf.name_scope("filter_of_conv2"):
-			self.W3 = self._weight_variable(shape=[self.filter_size, self.filter_size,
-				self.dim_layer2, self.dim_layer3])
-		with tf.name_scope("param_of_last_fullconect_layer"):
-			self.W4 = self._weight_variable(shape=[self.filter_size, self.filter_size,
-				self.dim_layer3, 1])
-
-	def _weight_variable(self, shape):
-		return tf.Variable(self.initializer(shape=shape))
-
-	def _bias_variable(self, shape):
-		return tf.Variable(self.initializer(shape=shape))
-
 	def _global_average_pool(self, x):
 		for _ in range(2):
 			x = tf.reduce_mean(x, axis=[1])
 		return x
 
-	def inference(self, input_tensor, name):
-		strides = [1, 2, 2, 1]
-		with tf.name_scope(name):
-			# conpute batch size
-			with tf.name_scope("conv_layer0"):
-				z = tf.nn.conv2d(input_tensor, self.W1, strides=strides, padding="SAME")
-				#z_bn = tf.layers.batch_normalization(z, training=self.is_training)
-				z_bn = tf.nn.dropout(z, self.keep_prob)
-				activation = tf.nn.leaky_relu(z)
-			#batch_size = tf.shape(activation)[0]
-			with tf.name_scope("conv_layer1"):
-				z1 = tf.nn.conv2d(activation, self.W2, strides=strides, padding="SAME")
-				#z1_bn = tf.layers.batch_normalization(z1, training=self.is_training)
-				z1_bn = tf.nn.dropout(z1, self.keep_prob)
-				activation = tf.nn.leaky_relu(z1)
-			with tf.name_scope("conv_layer2"):
-				z2 = tf.nn.conv2d(activation, self.W3, strides=strides, padding="SAME")
-				#z2_bn = tf.layers.batch_normalization(z2, training=self.is_training)
-				z2_bn = tf.nn.dropout(z2, self.keep_prob)
-				activation = tf.nn.leaky_relu(z2)
-			with tf.name_scope("layer3"):
-				z3 = tf.nn.conv2d(activation, self.W4, strides=strides, padding="SAME")
-				z3_ = self._global_average_pool(z3)
-				activation = tf.nn.sigmoid(z3_)
+	def inference(self, input_tensor, reuse=False):
+		activation = input_tensor
+		for i in range(self.n_layer):
+			with tf.variable_scope("conv{}".format(i), reuse=reuse):
+				w_shape = [self.kernels[i], self.kernels[i], self.in_channels[i], self.out_channels[i]]
+				# if variable that have same scope exits, get that variable(reuse)
+				W = tf.get_variable("weights", shape=w_shape,
+					dtype=tf.float32, initializer=self.w_initializer)
+				b = tf.get_variable("bias", shape=[self.out_channels[i]],
+					dtype=tf.float32, initializer=self.b_initializer)
+
+				z = tf.nn.conv2d(activation, W, strides=[1, self.kernels[i], self.kernels[i], 1],
+					padding="SAME")
+
+				z = tf.nn.bias_add(z, b)
+				# used batch norm except first layer
+				if i != 0:
+					with tf.name_scope("batch_norm"):
+						mean, variance = tf.nn.moments(z, axes=[0, 1, 2])
+						z = tf.nn.batch_normalization(z, mean, variance, None, None, 1e-08)
+				with tf.name_scope("leaky_relu"):
+					activation = tf.nn.leaky_relu(z)
+
+		with tf.variable_scope("dense_layer{}".format(self.n_layer), reuse=reuse):
+			w_shape = [self.out_channels[-1] * self.widths[-1] * self.widths[-1], self.out_dim]
+			# if variable that have same scope exits, get that variable(reuse)
+			W = tf.get_variable("weights", shape=w_shape,
+				dtype=tf.float32, initializer=self.w_initializer)
+			b = tf.get_variable("bias", shape=[self.out_dim],
+				dtype=tf.float32, initializer=self.b_initializer)
+
+			activation = tf.layers.flatten(activation)
+			z = tf.matmul(activation, W) + b
+			with tf.name_scope("batch_norm"):
+				mean, variance = tf.nn.moments(z, axes=[0])
+				z = tf.nn.batch_normalization(z, mean, variance, None, None, 1e-08)
+			with tf.name_scope("sigmoid"):
+				activation = tf.nn.sigmoid(z)
+
 		return activation
 
 
@@ -84,11 +84,11 @@ def main():
 
 	with tf.Graph().as_default():
 		with tf.name_scope("Discriminator"):
-			conv_net = CNNonlyConv(h_in=28, w_in=28, dim_in=1)
+			conv_net = CNNonlyConv()
 			with tf.name_scope("input"):
 				shape = np.append([None], xs.shape[1:])
 				x = tf.placeholder(tf.float32, shape=shape)
-			logit = conv_net.inference(x, "inference")
+			logit = conv_net.inference(x)
 
 		init = tf.global_variables_initializer()
 		with tf.Session() as sess:
@@ -102,6 +102,7 @@ def main():
 			sess.run(init)
 			ans = sess.run(logit, feed_dict={x: xs, conv_net.is_training: False, conv_net.keep_prob: 1.0})
 			print(ans.shape)
+
 
 if __name__ == '__main__':
 	main()
